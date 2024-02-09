9f0af70064a0a7039c615d5ed067e0bf7f4aaff7
ztaubaldiyev <zhanibek.tau@gmail.com>
2024-02-09 15:45:49 +0600

    fix phone

--- model/client.go
+++ model/client.go
@@ -8,6 +8,7 @@ type Client struct {
 	Lastname  string     `json:"lastname"`
 	Email     string     `json:"email"`
 	Password  string     `json:"password"`
+	Phone     string     `json:"phone"`
 	Token     string     `json:"token"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at"`
